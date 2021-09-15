6a76106ebfbaacb970e6efbf257a1a26762f2047
Gopinath Mano <gopimano4242@gmail.com>
2021-09-15 13:12:14 +0530

    new

---
+++ build/precache-manifest.d54c6ff64975ec27fab997594e61c645.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1f8bbc01b2459949bdafca9a08143b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "827eb1a2562b8ac82a9c",
+    "url": "/static/js/2.7aea2ed3.chunk.js"
+  },
+  {
+    "revision": "8d5be963884f6c58e0ce",
+    "url": "/static/js/main.e55d327d.chunk.js"
+  },
+  {
+    "revision": "05c790a9a60e4d4b4be2",
+    "url": "/static/js/runtime-main.a8757f30.js"
+  }
+]);
